2c486f0dc305eedb87efa5f29bc49d04447de00c
BONGRUCHAO <87805850+BONGRUCHAO@users.noreply.github.com>
2026-06-16 23:48:11 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -6,18 +6,25 @@ def crawl_news():
 
     r = requests.get(url, timeout=10)
 
+    print("STATUS:", r.status_code)
+    print("CONTENT SIZE:", len(r.content))
+
     root = ET.fromstring(r.content)
+    items = root.findall(".//item")
+
+    print("RSS ITEMS:", len(items))
 
     news = []
 
-    for item in root.findall(".//item"):
-        title = item.find("title").text
-        link = item.find("link").text
+    for i in items[:5]:
+        title = i.find("title").text
+        link = i.find("link").text
 
-        if title and link:
-            news.append({
-                "title": title,
-                "link": link
-            })
+        print("TITLE:", title)
 
-    return news[:15]
+        news.append({
+            "title": title,
+            "link": link
+        })
+
+    return news
