44696b720697214e0be7fcbb13c165b496176ac1
BONGRUCHAO <87805850+BONGRUCHAO@users.noreply.github.com>
2026-06-16 23:52:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
-from db import init_db, get_news
+from crawler import crawl_news
+from db import init_db, insert_news, get_news
 
 app = Flask(__name__)
 init_db()
 
 @app.route("/")
 def index():
+
+    news_list = crawl_news()
+
+    print("CRAWLED:", len(news_list))
+
+    for n in news_list:
+        insert_news(n["title"], n["link"], "", "")
+
     news = get_news()
+
     return render_template("index.html", news=news)
 
 if __name__ == "__main__":
