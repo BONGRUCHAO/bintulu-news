5ba321ce9160b0430124dc8f074cf4fd4f4da72a
BONGRUCHAO <87805850+BONGRUCHAO@users.noreply.github.com>
2026-06-16 22:58:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
 from flask import Flask, render_template
-from db import init_db, insert_news, get_news
-from crawler import crawl_news
-from ai import summarize
+from db import init_db, get_news
 
 app = Flask(__name__)
-
 init_db()
-
-def update_data():
-    news_list = crawl_news()
-
-    for n in news_list:
-        summary = summarize(n["title"])
-        insert_news(n["title"], n["link"], "", summary)
 
 @app.route("/")
 def index():
-    update_data()
     news = get_news()
     return render_template("index.html", news=news)
 
