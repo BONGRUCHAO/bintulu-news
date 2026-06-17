cca39784d59958541d7d90ef7f0e163ac3ff7946
BONGRUCHAO <87805850+BONGRUCHAO@users.noreply.github.com>
2026-06-17 22:45:39 +0800

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,17 +1,37 @@
-from flask import Flask, render_template
-from db import init_db, get_news
+import time
+from crawler import crawl_news
+from db import init_db, insert_news
+from ai import analyze
 
-app = Flask(__name__)
 init_db()
 
+def run_job():
+    print("=== START JOB ===")
 
-@app.route("/")
-def index():
-    news = get_news()
-    print("NEWS COUNT:", len(news))
+    news_list = crawl_news()
 
-    return render_template("index.html", news=news)
+    print("CRAWLED:", len(news_list))
+
+    for n in news_list:
+
+        category, summary = analyze(n["title"])
+
+        insert_news(
+            n["title"],
+            n["link"],
+            "",
+            summary,
+            category
+        )
+
+        print("SAVED:", n["title"])
+
+    print("=== JOB DONE ===")
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+while True:
+    run_job()
+
+    print("SLEEP 30 MIN...")
+    time.sleep(1800)
+    
