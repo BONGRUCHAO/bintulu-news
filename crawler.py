adfa5e4399db16258e1c85f76f4021e88f96f186
BONGRUCHAO <87805850+BONGRUCHAO@users.noreply.github.com>
2026-06-18 09:42:13 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -25,9 +25,14 @@ def get_content(url):
 def crawl_news():
     feed = feedparser.parse("https://www.thestar.com.my/rss")
 
+    print("RSS ITEMS:", len(feed.entries))
+
     news = []
 
     for e in feed.entries[:10]:
+
+        print("TITLE:", e.title)
+
         news.append({
             "title": e.title,
             "link": e.link,
