0cb36badc2e938328f255e247040a8008b46c6dc
BONGRUCHAO <87805850+BONGRUCHAO@users.noreply.github.com>
2026-06-18 09:48:43 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -23,8 +23,9 @@ def get_content(url):
 
 
 def crawl_news():
-    feed = feedparser.parse("https://www.thestar.com.my/rss")
-
+   feed = feedparser.parse(
+    "https://news.google.com/rss/search?q=Bintulu&hl=en-MY&gl=MY&ceid=MY:en"
+)
     print("RSS ITEMS:", len(feed.entries))
 
     news = []
