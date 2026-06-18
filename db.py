ead768c8bf2c3a9ee81ddae3f6cdcdd0cfd309c2
BONGRUCHAO <87805850+BONGRUCHAO@users.noreply.github.com>
2026-06-18 10:54:34 +0800

    Update db.py

--- db.py
+++ db.py
@@ -49,7 +49,7 @@ def get_news():
     c = conn.cursor()
 
     c.execute("""
-    SELECT title, link, summary, category, time
+    SELECT title, link, summary, category, published_time, time
     FROM news
     ORDER BY id DESC
     """)
